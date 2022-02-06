b9c3caa87739170f3e6b7177bab4fd2d049aae71
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-02-06 22:31:58 +0300

    added search section

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from "react";
 import Logo from "./Logo/Logo";
+import { SearchIcon } from "@heroicons/react/solid";
 
 type Props = {};
 
 const Header: FC = (props: Props): JSX.Element => {
   return (
-    <nav className="bg-white shadow shadow-gray-100 py-3">
+    <nav className="bg-white shadow shadow-gray-100 py-3 flex justify-between px-10 items-center">
       <Logo />
-      <div id="search"></div>
-      <div id="accounts"></div>
+      <div id="search" className="w-full flex justify-center">
+        <div className="w-full md:max-w-md lg:max-w-lg xl:max-w-xl flex items-center relative">
+          <input
+            type="text"
+            className="bg-gray-100 shadow shadow-gray-100 py-2 pl-5 pr-11 rounded w-full focus:outline-none tracking-wider text-gray-700 font-nunito"
+            placeholder="Find campgrounds here..."
+          />
+          <SearchIcon className="w-5 h-5 text-gray-500 absolute right-3 cursor-pointer"></SearchIcon>
+        </div>
+      </div>
+      <div id="accounts">accounts</div>
     </nav>
   );
 };
