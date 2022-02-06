cf0acc23d6ee20cfa674a2a79595880bf6a41bb1
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-02-06 22:38:56 +0300

    added accounts section

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import Logo from "./Logo/Logo";
-import { SearchIcon } from "@heroicons/react/solid";
+import Search from "./Search/Search";
 
 type Props = {};
 
@@ -8,17 +8,15 @@ const Header: FC = (props: Props): JSX.Element => {
   return (
     <nav className="bg-white shadow shadow-gray-100 py-3 flex justify-between px-10 items-center">
       <Logo />
-      <div id="search" className="w-full flex justify-center">
-        <div className="w-full md:max-w-md lg:max-w-lg xl:max-w-xl flex items-center relative">
-          <input
-            type="text"
-            className="bg-gray-100 shadow shadow-gray-100 py-2 pl-5 pr-11 rounded w-full focus:outline-none tracking-wider text-gray-700 font-nunito"
-            placeholder="Find campgrounds here..."
-          />
-          <SearchIcon className="w-5 h-5 text-gray-500 absolute right-3 cursor-pointer"></SearchIcon>
-        </div>
+      <Search />
+      <div id="accounts" className="flex w-full max-w-xs gap-3 font-nunito">
+        <button className="font-semibold tracking-wider text-sm">
+          Sign in
+        </button>
+        <button className="ml-2 tracking-wider text-sm font-semibold text-white bg-yellow-500 px-2 rounded-md py-1">
+          Create account
+        </button>
       </div>
-      <div id="accounts">accounts</div>
     </nav>
   );
 };
