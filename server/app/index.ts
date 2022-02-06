d6613ed1dcbf3e62cadef50bd90475804403f26b
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-02-06 11:19:27 +0300

    server init

---
+++ server/app/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import http from "http";
+
+import { resolvers, typeDefs } from "./graphql";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+
+const appGetStart = async () => {
+  const port = process.env.PORT;
+  const app = express();
+
+  // middlewares
+  app.use(express.json());
+  app.disable("x-powered-by");
+
+  // http server
+  const httpServer = http.createServer(app);
+
+  // server init
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+  httpServer.listen(port, () => {
+    console.log(`@ SERVER RUNNING PORT : ${port}`);
+  });
+};
+
+export default { appGetStart };
