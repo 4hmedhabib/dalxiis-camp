d355efbc537c8816468486b4eabb63af7e770bcd
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-02-07 16:35:33 +0300

    added modal ui

---
+++ client/src/components/UI/Modal/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+type Props = {};
+
+const Modal: FC = (props: Props): JSX.Element => {
+  return (
+    <Fragment>
+      {createPortal}
+      {createPortal}
+    </Fragment>
+  );
+};
+
+export default Modal;
